ec413b60931d34f9b724bafc465e2b30f5baea50
Ola Vea <ola@olavea.com>
2018-01-17 15:37:40 +0100

    Update posts.js
    Task #4 Hydrate with data from batcatdad

--- Client/data/posts.js
+++ Client/data/posts.js
@@ -1,22 +1,22 @@
 const posts = [
    {
       "code":"BAcyDyQwcXX",
-      "caption":"Lunch #hamont",
-      "likes":56,
+      "caption":"I am learning React.js and Redux with a great video course by @wesbos called Learn Redux. You find it at https://learnredux.com",
+      "likes":24,
       "id":"1161022966406956503",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/5e53d7473cd92d8162caab6bab1a4b79/5AF078AA/t51.2885-15/e35/26868214_1348431905268961_5798904889887162368_n.jpg"      
    },
    {
       "code":"BAcJeJrQca9",
       "caption":"In the morning all crows are black. #crow",
-      "likes":59,
+      "likes":17,
       "id":"1160844458347054781",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/c1cdf72b7d5736221b0f3c0ad5501dcc/5AEE887E/t51.2885-15/s640x640/sh0.08/e35/26072738_1778607539102181_4054382761589342208_n.jpg"
    },
    {
       "code":"BAF_KY4wcRY",
       "caption":"Owl mum, tractor, car, owl baby og gull. Lillian Raae-Vea (3) told me what to draw.",
-      "likes":79,
+      "likes":12,
       "id":"1154606670337393752",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/8a468e162cf84f1b93d1eb142fc01b46/5ADFE7B9/t51.2885-15/s640x640/sh0.08/e35/26156894_1956221098039360_3770277207604199424_n.jpg"
    },
@@ -33,6 +33,13 @@ const posts = [
       "likes":22,
       "id":"1126293663140399053",
       "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/38eafd7d5843610e91c2b16eb7baa4fe/5AE75871/t51.2885-15/s640x640/sh0.08/e35/26295729_156490118325358_1763657345767833600_n.jpg"
+   },
+   {
+      "code":"-B3eiIwcYV",
+      "caption":"This mornings doodles. #todaysdoodle #sketchnotes  #daddaughtertime #daddaughterdoodle",
+      "likes":29,
+      "id":"1117418173361145365",
+      "display_src":"https://scontent-arn2-1.cdninstagram.com/vp/05487bd59d0c4bb0b6f8a150ff672dd6/5AFCC07A/t51.2885-15/s640x640/sh0.08/e35/26151204_394354820977031_5927894723564929024_n.jpg"
    }
 ];
 
